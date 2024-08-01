947f77a4206ec37b0e13ed57823d7a30afc2f436
meligavincent <meligavincenttheophane@gmail.com>
2024-08-01 23:39:57 +0100

    list views

--- bidy/order_food/models.py
+++ bidy/order_food/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return str(self.name)
+    
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='foods')
+    def __str__(self):
+        return str(self.name)
+    
+
--- bidy/order_food/views.py
+++ bidy/order_food/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Food
 # Create your views here.
+
+class FoodListView(ListView):
+    model = Food
