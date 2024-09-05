11da0fbf3972d965d61b068766bc165957286203
meligavincent <meligavincenttheophane@gmail.com>
2024-09-05 09:04:33 +0100

    removed the BiddyBot from this repository

--- Biddy/Biddy/settings.py
+++ Biddy/Biddy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-np2yl!1y81ca1vh0h771k3+1%)og1d3(reykqzyv0&@(*=h&5v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
