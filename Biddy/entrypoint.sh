d7686a9b974f29db62d828f2d998b73e76cc7bbf
Senbonsakurakeyoshi1 <699580621meliga@gmail.com>
2024-09-19 09:54:30 +0100

    loaded data after server is completly started

--- Biddy/entrypoint.sh
+++ Biddy/entrypoint.sh
@@ -13,4 +13,12 @@ python manage.py load_yaml_data data.yaml
 python create_superuser.py
 
 # Lancer Gunicorn
-gunicorn Biddy.wsgi:application --bind 0.0.0.0:8000
+# Lancer Gunicorn en arrière-plan
+gunicorn Biddy.wsgi:application --bind 0.0.0.0:8000 &
+
+# Exécuter le script d'upload automatique après un délai pour s'assurer que l'application est prête
+sleep 30  # Attendre 30 secondes avant d'exécuter le script d'upload (ajustez le temps si nécessaire)
+python manage.py load_yaml_data data.yaml # Assurez-vous que ce script est créé pour gérer l'upload automatique
+
+# Garder le conteneur en cours d'exécution
+wait
